60a28f2694ceb982a0f900c3e2b8ca921f4b55c8
edoardo <pazedoardo@gmail.com>
2021-10-09 20:39:04 -0300

    Fazendo os testes

--- src/components/topicTwo/topicTwo.jsx
+++ src/components/topicTwo/topicTwo.jsx
@@ -7,7 +7,7 @@ export function topicTwo(props) {
 return <Main icon="home" title="Teste" subtitle="Teste3">
     <div className="display-4">Topico 2 do trabalho</div>
     <hr />
-    <p className="mb-0">Aqui vai o subtitulo</p>
+    <p className="mb-0">Fala do mateus. Exemplo</p>
 </Main>
 }
      
