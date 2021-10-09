a5bc7536723ece5cdf929dd78921afc87a6bac59
edoardo <pazedoardo@gmail.com>
2021-10-09 20:47:26 -0300

    Ajustando a SPA

--- Github/src/components/topicTwo/topicTwo.jsx
+++ Github/src/components/topicTwo/topicTwo.jsx
@@ -7,7 +7,7 @@ export function topicTwo(props) {
 return <Main icon="home" title="Teste" subtitle="Teste3">
     <div className="display-4">Topico 2 do trabalho</div>
     <hr />
-    <p className="mb-0">Fala do mateus. Exemplo</p>
+    <p className="mb-0">Fala do Edoardo. <span className="fa fa-heart"></span> Exemplo</p>
 </Main>
 }
      
